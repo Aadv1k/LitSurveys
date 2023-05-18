35c6e213834a73449ee7b9a46c89299731fff103
Aadv1k <aadv1k@outlook.com>
2023-05-19 01:07:16 +0530

    Remove accidental push of JS files

--- packages/backend/src/models/bootstrap.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var sequelize_1 = require("sequelize");
-var const_1 = require("../const");
-var connection_url = "postgresql://".concat(const_1.PG_CONFIG.username, ":").concat(const_1.PG_CONFIG.password, "@").concat(const_1.PG_CONFIG.host, ":").concat(const_1.PG_CONFIG.port, "/").concat(const_1.PG_CONFIG.dbName, "?sslmode=require");
-exports.default = new sequelize_1.Sequelize(connection_url);
