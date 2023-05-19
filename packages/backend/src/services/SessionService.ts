59ec185dd2c25bb201bee9b5d83f2acd6b693a94
Aadv1k <aadv1k@outlook.com>
2023-05-19 11:47:19 +0530

    Remove redundant files

--- packages/backend/src/services/SessionService.ts
+++ packages/backend/src/services/SessionService.ts
@@ -1,46 +1,45 @@
-import { createClient } from "redis";
-import { REDIS_CONFIG } from "../const";
+import { createClient } from 'redis'
+import { REDIS_CONFIG } from '../const'
 
 class SessionService {
-	client: any;
+  client: any
 
-	constructor() {
-		this.client = createClient({
-			socket: {
-				host: REDIS_CONFIG.host,
-				port: REDIS_CONFIG.port,
-			},
-			username: REDIS_CONFIG.username || "",
-			password: REDIS_CONFIG.password || ""
-			
-		});
-	}
+  constructor() {
+    this.client = createClient({
+      socket: {
+        host: REDIS_CONFIG.host,
+        port: REDIS_CONFIG.port
+      },
+      username: REDIS_CONFIG.username || '',
+      password: REDIS_CONFIG.password || ''
+    })
+  }
 
-	async init() {
-		await this.client.connect();
-	}
+  async init() {
+    await this.client.connect()
+  }
 
-	async pushSession(key: string, sessionInfo: any): Promise<string> {
-		try {
-			const resp = await this.client.set(key, JSON.stringify(sessionInfo));
-			return key;
-		} catch (err) {
-			throw err;
-		}
-	}
+  async pushSession(key: string, sessionInfo: any): Promise<string> {
+    try {
+      const resp = await this.client.set(key, JSON.stringify(sessionInfo))
+      return key
+    } catch (err) {
+      throw err
+    }
+  }
 
-	async getSession(key: string): Promise<Array<boolean> | null> {
-		try {
-			const resp = await this.client.get(key)
-			return JSON.parse(resp ?? "{}");
-		} catch (err) {
-			throw err;
-		}
-	}
+  async getSession(key: string): Promise<Array<boolean> | null> {
+    try {
+      const resp = await this.client.get(key)
+      return JSON.parse(resp ?? '{}')
+    } catch (err) {
+      throw err
+    }
+  }
 
-	async close() {
-		this.client.close();
-	}
+  async close() {
+    this.client.close()
+  }
 }
 
-export default new SessionService();
+export default new SessionService()
