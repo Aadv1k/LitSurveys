4c6aa0f950cecb16e824ff28b057acf9b8bf9aa3
Aadv1k <aadv1k@outlook.com>
2023-05-20 15:54:52 +0530

    Add field constants in common

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -2,6 +2,11 @@ export const SURVEY_MAX_TITLE_LEN = 255
 export const SURVEY_MAX_DESC_LEN = 600
 export const SURVEY_MAX_RESPONSES = 500
 
+export const FIELD = {
+  MAX_QUESTION_LENGTH: 255,
+  MAX_FIELDS: 32
+} 
+
 export * from './types/field'
 export * from './types/survey'
 export * from './types/user'
--- packages/common/types/field.ts
+++ packages/common/types/field.ts
@@ -7,7 +7,6 @@ export enum FieldType {
 export interface Field {
   id: string
   survey_id: string
-  user_id: string
   type: FieldType
   question: string
   response_options?: Array<string>
