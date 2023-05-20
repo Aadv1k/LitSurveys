8f56b2ba1d0601b715b1660a1d445fc0047955c5
Aadv1k <aadv1k@outlook.com>
2023-05-20 14:07:41 +0530

    Refactor the `@litsurvey/common` to be more modular

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -1,4 +1,8 @@
+export const SURVEY_MAX_TITLE_LEN = 255
+export const SURVEY_MAX_DESC_LEN = 600
+export const SURVEY_MAX_RESPONSES = 500
+
 export * from './types/field'
-export * from './types/response'
 export * from './types/survey'
 export * from './types/user'
+export * from './types/response'
--- packages/common/types/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserType = void 0;
-var UserType;
-(function (UserType) {
-    UserType["surveyor"] = "surveyor";
-    UserType["surveyee"] = "surveyee";
-    UserType["any"] = "any";
-})(UserType = exports.UserType || (exports.UserType = {}));
